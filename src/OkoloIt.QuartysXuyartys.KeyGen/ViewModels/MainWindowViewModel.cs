fd0e6eb24db1705877b00c15090a312c63caabe7
Ihor <egorwolf2k18@mail.ru>
2025-01-03 12:41:21 +0300

    feat: добавлена логика работы

--- src/OkoloIt.QuartysXuyartys.KeyGen/ViewModels/MainWindowViewModel.cs
+++ src/OkoloIt.QuartysXuyartys.KeyGen/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,10 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System;
+
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 using OkoloIt.QuartysXuyartys.KeyGen.Models;
 
@@ -6,13 +12,29 @@ namespace OkoloIt.QuartysXuyartys.KeyGen.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public MainWindowViewModel()
-    {
-        KeyGenerator keyGenerator = new();
-        string key = keyGenerator.Generate("C787");
-
-    }
+    private readonly KeyGenerator _keyGenerator = new();
 
     [ObservableProperty]
-    private string _message = "Welcome to QuartysXuyartys KeyGen!";
+    private string _installationCode = string.Empty;
+
+    [ObservableProperty]
+    private string _activationCode = string.Empty;
+
+    [RelayCommand]
+    private void OnGenerate()
+    {
+        try {
+            ActivationCode = _keyGenerator.Generate(InstallationCode);
+        }
+        catch (Exception) {
+            ActivationCode = "Ошибка генерации!";
+        }
+    }
+
+    [RelayCommand]
+    private void OnExit()
+    {
+        if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktopApp)
+            desktopApp.Shutdown();
+    }
 }
